17af313c6aacf8f362281247c5267918cfd063a6
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2024-11-13 16:41:38 +0600

    fix slider image issue

--- src/Pages/Home/Home_Components/Home_Slider_Section/Home_Slider_Section.jsx
+++ src/Pages/Home/Home_Components/Home_Slider_Section/Home_Slider_Section.jsx
@@ -1,64 +1,52 @@
 import Home_Product_Category from "../Home_Product_Category/Home_Product_Category";
 import Slider from "../Slider/Slider";
 
-
 const Home_Slider_Section = () => {
-    const slides = [
-        {
-          image: "https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/computer_essential_4_app_banner_copy_1_.jpg",
-        
-        },
-        {
-          image: "https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/unbeatableapp_banner.jpg",
-        
-        },
-        {
-          image: "https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/iPhone_16_Series_App_Banner.jpg",
-         
-        },
-        {
-            image: "https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/38__on_Refrigetor_app_banner_copy_2_.jpg",
-           
-          },
-          {
-            image: "https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/app_banner_3_copy.jpg",
-           
-          },
-          {
-            image:"https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/gadget_big_banner_4_1_.jpg"
-          }
-      ];
+  const slides = [
+    {
+      image:
+        "https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/computer_essential_4_app_banner_copy_1_.jpg",
+    },
+    {
+      image:
+        "https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/unbeatableapp_banner.jpg",
+    },
+    {
+      image:
+        "https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/iPhone_16_Series_App_Banner.jpg",
+    },
+    {
+      image:
+        "https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/38__on_Refrigetor_app_banner_copy_2_.jpg",
+    },
+    {
+      image:
+        "https://storage.googleapis.com/pickaboo-prod/media/dcastalia_hybridslider/image/app_banner_3_copy.jpg",
+    },
+  ];
   return (
-   <>
-
-<div>
-          <div className="grid grid-cols-12">
-            <div className=" col-span-12 lg:col-span-3">
-              <div className="flex flex-row md:flex-col gap-5 justify-center mb-5 lg:mb-5">
+    <>
+      <div>
+        <div className="container mx-auto grid grid-cols-12">
+          <div className=" col-span-12 lg:col-span-3">
+            <div className="flex flex-row md:flex-col gap-5 justify-center mb-5 lg:mb-5">
+              <div>
+                <div className=" border-[#71d0f0] border-l-8">sds</div>
                 <div>
-                  <div className=" border-[#71d0f0] border-l-8">
-                    sds
-                  </div>
-                  <div>
-                    <Home_Product_Category/>
-                  </div>
+                  <Home_Product_Category />
                 </div>
-                
               </div>
             </div>
-            <div className="col-span-12 lg:col-span-9 ">
-              <div className="p-2 md:p-0 ">
-                <Slider slides={slides}  />
-              </div>
-             
-            
+          </div>
+          <div className="col-span-12 lg:col-span-9 ">
+            <div className="h-[300px] md:h-[300px] w-full bg-red-500">
+              <Slider slides={slides} />
             </div>
           </div>
         </div>
-   
-   
-   </>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Home_Slider_Section
+export default Home_Slider_Section;
--- src/Pages/Home/Home_Components/Slider/Slider.jsx
+++ src/Pages/Home/Home_Components/Slider/Slider.jsx
@@ -2,7 +2,6 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import './Style.css';
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 export default function Slider({ slides }) {
@@ -25,9 +24,19 @@ export default function Slider({ slides }) {
       >
         {slides.map((slide, index) => (
           <SwiperSlide key={index}>
-            <div className="swiper-slide">
-              <img src={slide.image} alt="" />
-             
+            <div className="relative">
+              <img src={slide.image} alt="" className="w-full h-[25rem]" />
+              <div className="absolute top-20 left-[3.5rem]">
+                <h1 className="text-4xl text-white font-bold mb-4">
+                  {slide.heading}
+                </h1>
+                <p className="text-lg max-w-lg text-white mb-8">
+                  {slide.description}
+                </p>
+                <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                  {slide.buttonText}
+                </button>
+              </div>
             </div>
           </SwiperSlide>
         ))}
